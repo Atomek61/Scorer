afe7f6e64a464193dc5951438736bd26d744a504
Jan-Erich Schirrmacher <jan@atomek.de>
2023-11-16 21:43:30 +0100

    css layout with grid and flex

--- res/c.cmd
+++ res/c.cmd
@@ -1,4 +1,4 @@
-call cc red #ff8800 #ff0000 #220000
+call cc red #ff4400 #ff0000 #220000
 call cc blue #a1f4ff #00ffff #002020
 call cc green #96ff89 #00ff00 #002000
-call cc yellow #fff589 #ffbb00 #202000
+call cc yellow #ffef65 #ffe927 #202000
